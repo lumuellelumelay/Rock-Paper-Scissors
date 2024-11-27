6c13b27b87866d0d1e692b5d147c95604356f011
lumuellelumelay <168370509+lumuellelumelay@users.noreply.github.com>
2024-11-28 04:30:20 +0800

    Adding simple UI

--- script.js
+++ script.js
@@ -1,7 +1,5 @@
 // Note: added features:
-// --> Tie functionality
 // --> Restart button: with bot or player reached 5 points
-// --> announcing the results during the game
 // --> optimize javascript
 
 const humanChoice = document.querySelector('#player-choice');
@@ -9,14 +7,14 @@ const scoreBoard = document.querySelector('.score-board');
 const playerScore = document.querySelector('.p-score');
 const cpuScore = document.querySelector('.b-score');
 
+const score = document.createElement('span');
+scoreBoard.appendChild(score);
+
 let humanScore = 0,
   botScore = 0;
 
 // for anouncing the winner
 const announceWinner = function (humanScore, botScore) {
-  const score = document.createElement('span');
-  scoreBoard.appendChild(score);
-
   if (humanScore === 5) {
     score.textContent = 'You Win!';
   } else if (botScore === 5) {
@@ -33,28 +31,42 @@ const computerChoice = function () {
 
 // playing round
 const playRound = function (humanChoice) {
+  const botChoice = computerChoice();
+
   switch (humanChoice) {
     case 'rock':
-      if (computerChoice() === 'scissors') {
+      if (botChoice === 'scissors') {
+        score.textContent = `You win! "Rock" beats "Scissors"`;
         return true;
-      } else if (computerChoice() === 'paper') {
+      } else if (botChoice === 'paper') {
+        score.textContent = `You lose! "Paper" beats "Rock"`;
         return false;
+      } else {
+        score.textContent = `Player and Bot are "Rock"! Its a TIE!`;
       }
       break;
 
     case 'paper':
-      if (computerChoice() === 'rock') {
+      if (botChoice === 'rock') {
+        score.textContent = `You win! "Paper" beats "Rock"`;
         return true;
-      } else if (computerChoice() === 'scissors') {
+      } else if (botChoice === 'scissors') {
+        score.textContent = `You lose! "Scissors" beats "Paper"`;
         return false;
+      } else {
+        score.textContent = `Player and Bot are "Paper"! Its a TIE!`;
       }
       break;
 
     case 'scissors':
-      if (computerChoice() === 'paper') {
+      if (botChoice === 'paper') {
+        score.textContent = `You win! "Scissors" beats "Paper"`;
         return true;
-      } else if (computerChoice() === 'rock') {
+      } else if (botChoice === 'rock') {
+        score.textContent = `You lose! "Rock" beats "Scissors"`;
         return false;
+      } else {
+        score.textContent = `Player and Bot are "Scissors"! Its a TIE!`;
       }
       break;
   }
@@ -67,7 +79,7 @@ const playGame = function (e) {
   if (results === true) {
     humanScore++;
     playerScore.textContent = humanScore;
-  } else {
+  } else if (results === false) {
     botScore++;
     cpuScore.textContent = botScore;
   }
